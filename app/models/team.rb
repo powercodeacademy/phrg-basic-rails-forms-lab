5c365a325416b1942f002628a597cf0d93c9f563
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 13:45:36 -0400

    Add validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,9 @@
 class Team < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :coach, presence: true
+  validates :pg, presence: true
+  validates :sg, presence: true
+  validates :pf, presence: true
+  validates :sf, presence: true
+  validates :c, presence: true
 end
