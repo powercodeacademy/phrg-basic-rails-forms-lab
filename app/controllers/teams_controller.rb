85ebdbc2ddfe0ef7366e04a6f0c40809446f6160
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 13:42:23 -0400

    Write show, new, create actions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,24 @@
 class TeamsController < ApplicationController
-  # TODO: Add your controller actions here
-  # You'll need a 'new' action to display the form
-  # You'll need a 'create' action to process the form submission
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  def new
+    @team = Team.new(team_params)
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to @team, notice: 'Team created successfully!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :coach, :pg, :sg, :pf, :sf, :c)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  resources :team, only: %i[show new create]
-  # You need:
-  # - A GET route to '/newteam' that goes to the teams controller new action
-  # - A POST route to '/team' that goes to the teams controller create action
+  resources :teams, only: %i[show new create]
 end
